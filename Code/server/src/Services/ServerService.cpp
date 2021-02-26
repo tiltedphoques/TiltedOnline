f1da91a461d08f4a66bb60db712931f359f7a217
dragonisser <dragonisser@googlemail.com>
2021-02-26 17:05:21 +0000

    Fix SendChatMessage

--- Code/server/src/Services/ServerService.cpp
+++ Code/server/src/Services/ServerService.cpp
@@ -17,11 +17,11 @@ uint32_t ServerService::GetId() const noexcept
 void ServerService::SendChatMessage(Script::Player aPlayer, const std::string aMessage)
 {
     spdlog::debug(aMessage);
-    //NotifyChatMessageBroadcast notifyMessage;
-    //notifyMessage.PlayerName = "";
-    //notifyMessage.ChatMessage = aMessage;
+    NotifyChatMessageBroadcast notifyMessage;
+    notifyMessage.PlayerName = "";
+    notifyMessage.ChatMessage = aMessage;
 
-    //auto& playerComponent = m_pWorld->get<PlayerComponent>(aPlayer.GetEntityHandle());
-    //GameServer::Get()->Send(playerComponent.ConnectionId, notifyMessage);
+    auto& playerComponent = m_world.get<PlayerComponent>(aPlayer.GetEntityHandle());
+    GameServer::Get()->Send(playerComponent.ConnectionId, notifyMessage);
 }
 
