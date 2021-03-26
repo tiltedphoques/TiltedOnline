eca7393016470533515170c6f942ef904dcfb9e2
Dragonisser <dragonisser@googlemail.com>
2021-03-26 14:41:45 +0100

    Fix build error

--- Code/client/Services/Generic/OverlayClient.cpp
+++ Code/client/Services/Generic/OverlayClient.cpp
@@ -4,6 +4,7 @@
 #include <Services/OverlayClient.h>
 
 #include <Services/TransportService.h>
+#include <Messages/SendChatMessageRequest.h>
 
 OverlayClient::OverlayClient(TransportService& aTransport, TiltedPhoques::OverlayRenderHandler* apHandler)
     : TiltedPhoques::OverlayClient(apHandler), m_transport(aTransport)
